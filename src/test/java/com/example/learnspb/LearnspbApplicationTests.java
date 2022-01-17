05f21db402c0745238dd7056c65384f46746bd24
JiananYuan <yuanjiajiaqq@163.com>
2022-01-17 11:40:25 +0800

    feat: springboot之helloworld

---
+++ src/test/java/com/example/learnspb/LearnspbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnspb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
